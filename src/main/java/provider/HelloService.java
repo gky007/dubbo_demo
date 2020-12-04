51e8edcc32017f1c91e1ddfdb6ed96fca616b989
10844 <1084407178@qq.com>
2020-12-04 11:14:04 +0800

    Initial commit

---
+++ src/main/java/provider/HelloService.java
@@ -0,0 +1,10 @@
+package provider;
+
+/**
+ * @author jamie
+ * @version 1.0.0
+ * @date 2020/12/4 11:09
+ * @description
+ */
+public interface HelloService {
+}
---
+++ src/main/java/provider/api/HelloServiceImp.java
@@ -0,0 +1,10 @@
+package provider.api;
+
+/**
+ * @author jamie
+ * @version 1.0.0
+ * @date 2020/12/4 11:11
+ * @description
+ */
+public class HelloServiceImp {
+}
